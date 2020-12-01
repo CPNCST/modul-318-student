b16d421e8674f7ddb55802f55a0fe5a0752d5840
Administrator adm-virtedu <adm-virtedu@iet-gibb.ch>
2020-12-01 09:18:30 +0100

    StationBoard functionality implemented

--- SwissTransport_Projektarbeit/AbfahrtstafelForm.cs
+++ SwissTransport_Projektarbeit/AbfahrtstafelForm.cs
@@ -15,7 +15,6 @@ namespace SwissTransport_Projektarbeit
     {
         // Membervariablen 
         Transport _transport = new Transport();
-        List<String> stationID = new List<string>();
 
         public AbfahrtstafelForm()
         {
@@ -25,20 +24,56 @@ namespace SwissTransport_Projektarbeit
         private void abfahrtStationSuchenBtn_Click(object sender, EventArgs e)
         {
             abfahrtStationListView.Items.Clear();
+            abfahrtStationListView.Columns.Clear();
+            abfahrtStationListView.View = View.Details;
+            abfahrtStationListView.Items.AddRange(GetStationBoard(abfahrtStationTextBox.Text));
         }
 
-        private ListViewItem[] getStationConnections(string station, string ID)
+        private ListViewItem[] GetStationBoard(string fromStation)
         {
-            StationBoardRoot stationBoard = _transport.GetStationBoard(station, ID);
+            Stations stations = new Stations();
+            stations = _transport.GetStations(fromStation);
+            string stationID = stations.StationList.First().Id;
+            StationBoardRoot stationBoard = null;
 
-            ListViewItem[] listView = new ListViewItem[stationBoard.Entries.Count];
-
-            for (int i = 0; i < stationBoard.Entries.Count; i++)
+            try
             {
-                //listView[i] = new ListViewItem(stationBoard)
+                stationBoard = _transport.GetStationBoard(fromStation, stationID);
+            }
+            catch (Exception ex)
+            {
+                ListViewItem[] listViewError = new ListViewItem[1];
+                listViewError[0] = new ListViewItem("FEHLER");
+                listViewError[0].SubItems.Add(ex.Message);
+                
+                return listViewError;
             }
 
-            return listView;
+            AddColumns();
+            ListViewItem[] stationListView = new ListViewItem[stationBoard.Entries.Count];
+            int i = 0; 
+            
+            foreach (StationBoard item in stationBoard.Entries)
+            {
+                stationListView[i] = new ListViewItem(item.To); // Zu Station
+                stationListView[i].SubItems.Add(item.Stop.Departure.ToShortTimeString()); // Abfahrt 
+                stationListView[i].SubItems.Add(item.Name); // Zuginformation
+                i++;
+            }
+
+            return stationListView;
+        }
+
+        private void AddColumns()
+        {
+            abfahrtStationListView.Columns.Add("Station Name");
+            abfahrtStationListView.Columns.Add("Zu Station");
+            abfahrtStationListView.Columns.Add("Abfahrt");
+
+            for (int i = 0; i < abfahrtStationListView.Columns.Count; i++)
+            {
+                abfahrtStationListView.Columns[i].Width = 90;
+            }
         }
     }
 }
